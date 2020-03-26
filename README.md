7bb1045c66b6f8d17419e1b79aae7a5e155ae1c2
Kingsley <kingsleyumujeyan@gmail.com>
2020-03-26 20:13:45 +0100

    All files updated

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # javascript_startng
-    - StartNG Javascript Task-1
+
+JavaScript Task 1
+Deadline: Mar 27, 2020 12:00 AM
+Description
+
+Task: 
+
+
+Store your name in a variable. 
+
+Store your courses in an array. 
+
+Display your name and courses and do one of the following: 
+
+
+1. If the number of courses you are doing is an odd number, find and display all odd numbers from 1-200 (inclusive)
+2. If the number of courses you are doing is an even number, find and display all even numbers from 1-200 (inclusive)
--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let name = "Kingsley Umujeyan";
-let myCourses = ["Html","CSS","Javascript","NodeJS"];
+           const name = "Kingsley Umujeyan";
+           console.log(name);
+           let myCourses = ["Html","CSS","JavaScript","NodeJS"];
+           console.log(myCourses);
+           document.getElementById("name").innerHTML = name;
+           document.getElementById("courses").innerHTML = myCourses;
+           
+           function even(){
+                for( let i = 1;i <= 200;i++){
+                    if(i % 2 === 0){
+                   document.write(i + ' ');       
+                    }
+                };    
+            };
+            function odd(){
+                for( let i = 1;i <= 200;i++){
+                    if(i % 2 !== 0){
+                    document.write(i + ' ');       
+                    }
+                };    
+            };
 
+            if(myCourses.length % 2 === 0){
+                even();
+            }else{
+                 odd();
+            };
